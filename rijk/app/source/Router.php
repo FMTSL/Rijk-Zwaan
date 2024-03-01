00778d279ea4b23b1262e2521b8b9afe08a49174
Felipe Matos <Felipe_Matos@Hotmail.com.br>
2024-03-01 07:57:51 -0300

    Atualização do Router.php

--- rijk/app/source/Router.php
+++ rijk/app/source/Router.php
@@ -170,7 +170,6 @@ $route->get("/product/stock/{id}", "AppProductsStock:updateView");
 $route->post("/product/stock/update", "AppProductsStock:updateAction");
 $route->delete("/product/stock/delete", "AppProductsStock:deleteAction");
 
-
 $route->get("/product/stock/clone", "AppProductsStockClone:listView");
 $route->get("/product/stock/clone/lists/all", "AppProductsStockClone:listViewAll");
 $route->get("/product/list/clone/{id}", "AppProductsStockClone:listViewId");
@@ -179,6 +178,14 @@ $route->get("/product/stock/clone/{id}", "AppProductsStockClone:updateView");
 $route->post("/product/stock/clone/update", "AppProductsStockClone:updateAction");
 $route->delete("/product/stock/clone/delete", "AppProductsStockClone:deleteAction");
 $route->delete("/product/stock/clone/delete/all", "AppProductsStockClone:deleteAllAction");
+
+$route->get("/product/stock/euro", "AppProductsStockEuro:listView");
+$route->get("/product/stock/euro/lists/all", "AppProductsStockEuro:listViewAll");
+$route->get("/product/list/euro/{id}", "AppProductsStockEuro:listViewId");
+$route->post("/product/stock/euro/new", "AppProductsStockEuro:newAction");
+$route->get("/product/stock/euro/{id}", "AppProductsStockEuro:updateView");
+$route->post("/product/stock/euro/update", "AppProductsStockEuro:updateAction");
+$route->delete("/product/stock/euro/delete", "AppProductsStockEuro:deleteAction");
 
 
 $route->get("/bonus-order", "AppBonusOrder:listView");
@@ -321,6 +328,10 @@ $route->get("/export/products", "AppExportsProductsStock:newAction");
 $route->get("/import/products", "AppImportsProductsStock:listView");
 $route->post("/import/products/new", "AppImportsProductsStock:newAction");
 
+$route->get("/export/products/euro", "AppExportsProductsStockEuro:newAction");
+$route->get("/import/products/euro", "AppImportsProductsStockEuro:listView");
+$route->post("/import/products/new/euro", "AppImportsProductsStockEuro:newAction");
+
 $route->get("/export/products/clone", "AppExportsProductsStockClone:newAction");
 $route->get("/import/products/clone", "AppImportsProductsClone:listView");
 $route->post("/import/products/new/clone", "AppImportsProductsClone:newAction");
@@ -339,7 +350,7 @@ $route->get("/files/new", "AppFiles:listViewNew");
 $route->post("/files/new-action", "AppFiles:newAction");
 $route->delete("/files/delete", "AppFiles:deleteAction");
 
-
+$route->get("/get-euro-value/all", "AppOrderForm:getEuroValue");
 
 /**
  * ERROR
@@ -355,3 +366,5 @@ $route->dispatch();
 if ($route->error()) {
     $route->redirect("/ops/{$route->error()}");
 }
+
+
