e067ad4e885e633aaf3e067f7b04b1927c3a3e22
Felipe Matos <Felipe_Matos@Hotmail.com.br>
2024-03-01 23:29:06 -0300

    Update customers.js

--- rijk/app/theme/assets/js/pages/customers.js
+++ rijk/app/theme/assets/js/pages/customers.js
@@ -2,17 +2,14 @@
  * Form Ações
  */
 $("#form").submit(function (e) {
-  e.preventDefault(); // Impede o envio padrão do formulário
+  e.preventDefault();
 
-  // Obter o valor do campo euro
-  var euroValue = $("#euro").is(":checked") ? 1 : 0;
-  console.log("Euro Value:", euroValue);
-
-  // Adicionar o valor do campo euro ao FormData
   var formData = new FormData(this);
-  formData.append("euro", euroValue);
+  
+   for (var pair of formData.entries()) {
+    console.log(pair[0]+ ', ' + pair[1]); 
+  }
 
-  // Continuar com o envio do formulário usando AJAX
   $.ajax({
     url: $("#form").attr("action"),
     type: "post",
@@ -25,7 +22,7 @@ $("#form").submit(function (e) {
       );
     },
     success: function (dd) {
-      console.log("Success:", dd);
+      console.log("Success:", dd); 
       $("#form").trigger("reset");
       if (dd.resp > 0) {
         $.toast({
@@ -65,7 +62,7 @@ $("#form").submit(function (e) {
       }
     },
     error: function (dd) {
-      console.log("Error:", dd);
+      console.log("Error:", dd); 
       $("#form button.btn").show();
       $(".carrega").hide();
       $.toast({
@@ -90,81 +87,8 @@ $("#form").submit(function (e) {
   return false;
 });
 
-$("#formAddress").submit(function (e) {
-  var formData = new FormData(this);
-  $.ajax({
-    url: $("#formAddress").attr("action"),
-    type: "post",
-    data: formData,
-    beforeSend: function () {
-      console.log("Before Send");
-      $("#form button.btn").hide();
-      $(".carrega").html(
-        '<div class="env"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: transparent; display: block; shape-rendering: auto;" width="30px" height="30px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><path d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#a90e19" stroke="none"><animateTransform attributeName="transform" type="rotate" dur="1s" repeatCount="indefinite" keyTimes="0;1" values="0 50 51;360 50 51"></animateTransform></path><br/>Aguarde processando informações!</div>'
-      );
-    },
-    success: function (dd) {
-      console.log("Success:", dd);
-      $("#form").trigger("reset");
-      if (dd.resp > 0) {
-        $.toast({
-          text: dd.mensagem,
-          showHideTransition: "fade",
-          position: "top-right",
-          loader: false,
-          icon: "success",
-        });
-        if (dd.redirect) {
-          setTimeout(function () {
-            window.location.reload();
-          }, 500);
-        }
-        if (dd.modal) {
-          $(`#${dd.modal}`).modal("hide");
-        }
-      } else {
-        $.toast({
-          text: dd.mensagem,
-          showHideTransition: "fade",
-          position: "top-right",
-          loader: false,
-          icon: "error",
-        });
-        if (dd.redirect) {
-          setTimeout(function () {
-            window.location.reload();
-          }, 500);
-        }
-        $("button.btn").show();
-        $(".carrega").hide();
-      }
-    },
-    error: function (dd) {
-      console.log("Error:", dd);
-      $("#form button.btn").show();
-      $(".carrega").hide();
-      $.toast({
-        text: "Oops we had a problem!",
-        showHideTransition: "fade",
-        position: "top-right",
-        loader: false,
-        icon: "error",
-      });
-    },
-    cache: false,
-    contentType: false,
-    processData: false,
-    xhr: function () {
-      var myXhr = $.ajaxSettings.xhr();
-      if (myXhr.upload) {
-        myXhr.upload.addEventListener("progress", function () {}, false);
-      }
-      return myXhr;
-    },
-  });
-  return false;
-});
 function update(id) {
+  console.log("Updating address with ID:", id); 
   $.get(`/customer-address/${id}`, function (dd) {
     $("#new").modal("show");
     $("#formAddress #id_address").val(dd.id);
@@ -187,13 +111,14 @@ function update(id) {
   return false;
 }
 
+
 function deletarEnd(id) {
   $.ajax({
     url: `/customer-address/delete`,
     type: "delete",
     data: { id },
     success: function (dd) {
-      console.log("Success:", dd);
+      console.log("Success:", dd); 
       if (dd.resp > 0) {
         $.toast({
           text: dd.mensagem,
@@ -216,77 +141,74 @@ function deletarEnd(id) {
   return false;
 }
 
-$.get(`/customers/lists/all`, function (dd) {
-  var table = $("#root").tableSortable({
-    data: dd,
-    columns: {
-      full_name: "Name",
-      email: "Email",
-      mobile: "Phone",
-      cnpj: "CNPJ",
-      special_client: "Special Client",
-      euro: "Euro",
-      actions: "",
-    },
-    searchField: "#searchField",
-    responsive: {
-      1100: {
-        columns: {
-          full_name: "Name",
-          mobile: "Phone",
-          special_client: "Special Client",
-          euro: "Euro",
-          actions: "",
+$(document).ready(function() {
+  $.get(`/customers/lists/all`).then(function (dd) {
+    var table = $("#root").tableSortable({
+      data: dd,
+      columns: {
+        full_name: "Name",
+        email: "Email",
+        mobile: "Phone",
+        cnpj: "CNPJ",
+        special_client: "Special Client",
+        euro: "Euro",
+        actions: "",
+      },
+      searchField: "#searchField",
+      responsive: {
+        1100: {
+          columns: {
+            full_name: "Name",
+            mobile: "Phone",
+            special_client: "Special Client",
+            euro: "Euro",
+            actions: "",
+          },
         },
       },
-    },
-    rowsPerPage: 20,
-    pagination: true,
-    onPaginationChange: function (nextPage, setPage) {
-      setPage(nextPage);
-    },
-  });
-
-  $(document).ready(function() {
-    // Faz uma requisição AJAX para obter os detalhes do cliente específico
-    $.get('/customers/lists/all', function(customers) {
-        // Adiciona um evento de mudança ao filtro de euro
-        $("#euroFilter").on("change", function() {
-            var euroFilter = $(this).val();
-            var filteredCustomers = customers;
-
-            // Filtra os clientes com base no valor selecionado no filtro de euro
-            if (euroFilter !== "") {
-                filteredCustomers = customers.filter(function(customer) {
-                    return customer.euro.toString() === euroFilter;
-                });
-            }
-
-            // Atualiza a tabela com os dados filtrados
-            updateTable(filteredCustomers);
-        });
-
-        // Função para atualizar a tabela com os dados filtrados
-        function updateTable(data) {
-          var tableHtml = "<tr>";
-          data.forEach(function(customer) {
-              tableHtml += "<tr>";
-              tableHtml += "<td>" + customer.full_name + "</td>";
-              tableHtml += "<td>" + customer.email + "</td>";
-              tableHtml += "<td>" + customer.mobile + "</td>";
-              tableHtml += "<td>" + (customer.cnpj !== null ? customer.cnpj : '') + "</td>";
-              tableHtml += "<td>" + customer.special_client + "</td>";
-              tableHtml += "<td>" + (customer.euro ? 'Yes' : 'No') + "</td>";
-              tableHtml += "<td>" + customer.actions + "</td>";
-              tableHtml += "</tr>";
-          });
-          $("#root table tbody").html(tableHtml);
-        }
-        // Atualiza a tabela com todos os clientes ao carregar a página
-        updateTable(customers);
+      rowsPerPage: 20,
+      pagination: true,
+      onPaginationChange: function (nextPage, setPage) {
+        setPage(nextPage);
+      },
     });
-});
 
+    // Filtro de euro
+    $("#euroFilter").on("change", function() {
+      var euroFilter = $(this).val();
+      var filteredCustomers = dd;
+
+      // Filtra os clientes com base no valor selecionado no filtro de euro
+      if (euroFilter !== "") {
+        filteredCustomers = dd.filter(function(customer) {
+          return customer.euro.toString() === euroFilter;
+        });
+      }
+
+      // Atualiza a tabela com os dados filtrados
+      updateTable(filteredCustomers);
+    });
+
+    // Função para atualizar a tabela com os dados filtrados
+    function updateTable(data) {
+      var tableHtml = "<tr>";
+      data.forEach(function(customer) {
+        tableHtml += "<tr>";
+        tableHtml += "<td>" + customer.full_name + "</td>";
+        tableHtml += "<td>" + customer.email + "</td>";
+        tableHtml += "<td>" + customer.mobile + "</td>";
+        tableHtml += "<td>" + (customer.cnpj !== null ? customer.cnpj : '') + "</td>";
+        tableHtml += "<td>" + customer.special_client + "</td>";
+        tableHtml += "<td>" + (customer.euro ? 'Yes' : 'No') + "</td>";
+        tableHtml += "<td>" + customer.actions + "</td>";
+        tableHtml += "</tr>";
+      });
+      $("#root table tbody").html(tableHtml);
+    }
+
+    // Atualiza a tabela com todos os clientes ao carregar a página
+    updateTable(dd);
+  });
 
   $("#changeRows").on("change", function () {
     table.updateRowsPerPage(parseInt($(this).val(), 10));
@@ -296,8 +218,8 @@ $.get(`/customers/lists/all`, function (dd) {
     table.refresh(true);
   });
 
-  $("#distory").click(function () {
-    table.distroy();
+  $("#destroy").click(function () {
+    table.destroy();
   });
 
   $("#refresh").click(function () {
@@ -315,7 +237,7 @@ function deletar(id) {
     type: "delete",
     data: { id },
     success: function (dd) {
-      console.log("Success:", dd);
+      console.log("Success:", dd); 
       if (dd.resp > 0) {
         $.toast({
           text: dd.mensagem,
